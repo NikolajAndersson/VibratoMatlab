fb98fa50cb67c4ab77a21ab0700505a3a0173c16
NikolajAndersson <nikolajandersson@gmail.com>
2017-03-09 22:02:57 +0100

    added waveforms

--- Vibrato.m
+++ Vibrato.m
@@ -2,6 +2,7 @@ classdef Vibrato < handle
     properties
         rate
         width
+        waveform
     end
     properties (Access=private)
         delay
@@ -10,24 +11,29 @@ classdef Vibrato < handle
         BufferIndex = 1;
         phase = 0;
         freq
+        amplitude = 0.9;
     end
     
     methods
-        function obj = Vibrato(r_, w_, fs_)
+        function obj = Vibrato(r_, w_, waveform, fs_)
             obj.rate = r_;
             obj.width = w_;
             obj.fs = fs_;
             
             obj.delay = round(obj.width * obj.fs);
             obj.freq = obj.rate / obj.fs;
+            obj.waveform = waveform;
         end
         function setRate(obj,rate)
             obj.rate = rate;
-            obj.freq = rate / obj.fs;        
+            obj.freq = rate / obj.fs;
         end
         function setWidth(obj,width)
             obj.width = width;
             obj.delay = round(width * obj.fs);
+        end
+        function setWaveform(obj,waveform)
+            obj.waveform = waveform;
         end
         function out = process(obj, x)
             y = zeros(size(x));
@@ -38,16 +44,37 @@ classdef Vibrato < handle
             for i = 1:size(x,1)
                 obj.Buffer(writeIndex,:) = x(i,:); % Store buffer
                 
-                MOD = sin(obj.phase);
+                osc = sin(obj.phase);
+                
+                switch(obj.waveform)
+                    case 'Sine'
+                        osc = obj.amplitude*sin(obj.phase);
+                    case 'Square'
+                        if obj.phase < pi
+                            osc = obj.amplitude;
+                        else
+                            osc = -obj.amplitude;
+                        end
+                    case 'Sawtooth'
+                        osc = obj.amplitude - (obj.amplitude / pi * obj.phase);
+                    case 'Triangle'
+                        if obj.phase < pi
+                            osc = -obj.amplitude + (2 * obj.amplitude / pi)  * obj.phase;
+                        else
+                            osc = 3 * obj.amplitude - (2 * obj.amplitude / pi)  * obj.phase;
+                        end                      
+                end
                 
                 obj.phase = obj.phase + delta;
+                
                 if obj.phase > 2 * pi
                     obj.phase = obj.phase - 2*pi;
                 end
                 
-                TAP=1+obj.delay+obj.delay*MOD;
-                n=floor(TAP);
-                frac = TAP - n;
+                tap = 1 + obj.delay + obj.delay * osc;
+                
+                n=floor(tap);
+                frac = tap - n;
                 readIndex = floor(writeIndex - n);
                 
                 if readIndex <= 0
@@ -70,7 +97,8 @@ classdef Vibrato < handle
             obj.fs = fs;
             obj.BufferIndex = 1;
             obj.Buffer = zeros(192001,2);
+            obj.phase = 0; 
         end
         
-    end    
+    end
 end
--- VibratoPlugin.m
+++ VibratoPlugin.m
@@ -3,23 +3,21 @@ classdef VibratoPlugin < audioPlugin
     properties
         width = 0.0001
         rate = 9
+        waveform = 'Sine'
         Mix = 0.5
     end
     
     properties (Dependent)
     end
     
-    properties
-
-        
-    end
-    
     properties (Constant)
         % audioPluginInterface manages the number of input/output channels
         % and uses audioPluginParameter to generate plugin UI parameters.
         PluginInterface = audioPluginInterface(...
-            audioPluginParameter('width','DisplayName','width','Label','','Mapping',{'lin',0.00001 0.001}),...
-            audioPluginParameter('rate','DisplayName','Modulation Rate','Label','','Mapping',{'lin',1 14}),...
+            audioPluginParameter('width','DisplayName','Width','Label','','Mapping',{'lin',0.00001 0.001}),...
+            audioPluginParameter('rate','DisplayName','Modulation Rate','Label','Hz','Mapping',{'lin',0.1 14}),...
+            audioPluginParameter('waveform',...
+            'DisplayName','Waveform','Mapping',{'enum','Sine','Square', 'Sawtooth','Triangle'}),...
             audioPluginParameter('Mix','DisplayName','Dry/Wet','Label','%','Mapping',{'lin',0 1}));
     end
     
@@ -29,19 +27,23 @@ classdef VibratoPlugin < audioPlugin
     
     methods
         function obj = VibratoPlugin()
-           obj.vibrato = Vibrato(obj.rate, obj.width, getSampleRate(obj));         
+            obj.vibrato = Vibrato(obj.rate, obj.width, obj.waveform, getSampleRate(obj));
         end
         
         function set.rate(obj,rate)
             obj.rate = rate;
-           % obj.vibrato.rate = obj.rate; 
-           setRate(obj.vibrato,rate);
+            % obj.vibrato.rate = obj.rate;
+            setRate(obj.vibrato, obj.rate);
         end
-        
-        function set.width(obj,width)
+         function set.waveform(obj,waveform)
+            obj.waveform = waveform;
+            % obj.vibrato.rate = obj.rate;
+            setWaveform(obj.vibrato,waveform);
+        end
+        function set.width(obj, width)
             obj.width = width;
-           % obj.vibrato.width = obj.width;
-            setWidth(obj.vibrato,width);
+            % obj.vibrato.width = obj.width;
+            setWidth(obj.vibrato,obj.width);
         end
         
         function reset(obj)
@@ -49,7 +51,7 @@ classdef VibratoPlugin < audioPlugin
         end
         
         function out = process(obj, x)
-            y = zeros(size(x));
+           % y = zeros(size(x));
             y = process(obj.vibrato, x);
             
             % Calculate output by adding wet and dry signal in appropriate
