e38be21d763309b200d0182c449c7c55b4ace0e1
NikolajAndersson <nikolajandersson@gmail.com>
2017-03-09 20:10:29 +0100

    added class files

--- Vibrato.m
+++ Vibrato.m
@@ -1,3 +1,76 @@
-classdef Vibrato
+classdef Vibrato < handle
+    properties
+        rate
+        width
+    end
+    properties (Access=private)
+        delay
+        fs
+        Buffer = zeros(192001,2);
+        BufferIndex = 1;
+        phase = 0;
+        freq
+    end
     
+    methods
+        function obj = Vibrato(r_, w_, fs_)
+            obj.rate = r_;
+            obj.width = w_;
+            obj.fs = fs_;
+            
+            obj.delay = round(obj.width * obj.fs);
+            obj.freq = obj.rate / obj.fs;
+        end
+        function setRate(obj,rate)
+            obj.rate = rate;
+            obj.freq = rate / obj.fs;        
+        end
+        function setWidth(obj,width)
+            obj.width = width;
+            obj.delay = round(width * obj.fs);
+        end
+        function out = process(obj, x)
+            y = zeros(size(x));
+            writeIndex = obj.BufferIndex;
+            
+            delta = obj.freq * 2 * pi;
+            
+            for i = 1:size(x,1)
+                obj.Buffer(writeIndex,:) = x(i,:); % Store buffer
+                
+                MOD = sin(obj.phase);
+                
+                obj.phase = obj.phase + delta;
+                if obj.phase > 2 * pi
+                    obj.phase = obj.phase - 2*pi;
+                end
+                
+                TAP=1+obj.delay+obj.delay*MOD;
+                n=floor(TAP);
+                frac = TAP - n;
+                readIndex = floor(writeIndex - n);
+                
+                if readIndex <= 0
+                    readIndex = readIndex + 192001;
+                end
+                if readIndex == 1
+                    y(i,:) = frac*obj.Buffer(192001) + (1-frac)*obj.Buffer(readIndex);
+                else
+                    y(i,:) = frac*obj.Buffer(readIndex - 1) + (1-frac)*obj.Buffer(readIndex);
+                end
+                writeIndex = writeIndex + 1;
+                if writeIndex > 192001
+                    writeIndex =  1;
+                end
+            end
+            obj.BufferIndex = writeIndex;
+            out = y;
+        end
+        function reset(obj, fs)
+            obj.fs = fs;
+            obj.BufferIndex = 1;
+            obj.Buffer = zeros(192001,2);
+        end
+        
+    end    
 end
--- VibratoPlugin.m
+++ VibratoPlugin.m
@@ -1,8 +1,8 @@
 classdef VibratoPlugin < audioPlugin
     
     properties
-        Width = 0.0001
-        Modrate = 9
+        width = 0.0001
+        rate = 9
         Mix = 0.5
     end
     
@@ -10,11 +10,7 @@ classdef VibratoPlugin < audioPlugin
     end
     
     properties
-        DELAY
-        
-        WIDTH
-        
-        MODFREQ
+
         
     end
     
@@ -22,90 +18,39 @@ classdef VibratoPlugin < audioPlugin
         % audioPluginInterface manages the number of input/output channels
         % and uses audioPluginParameter to generate plugin UI parameters.
         PluginInterface = audioPluginInterface(...
-            audioPluginParameter('Width','DisplayName','Width','Label','','Mapping',{'lin',0.00001 0.001}),...
-            audioPluginParameter('Modrate','DisplayName','Modulation Rate','Label','','Mapping',{'lin',1 14}),...
+            audioPluginParameter('width','DisplayName','width','Label','','Mapping',{'lin',0.00001 0.001}),...
+            audioPluginParameter('rate','DisplayName','Modulation Rate','Label','','Mapping',{'lin',1 14}),...
             audioPluginParameter('Mix','DisplayName','Dry/Wet','Label','%','Mapping',{'lin',0 1}));
     end
     
     properties (Access = private)
-        pSR
-        
-        Buffer = zeros(192001,2);
-        BufferIndex = 1;
-%         NSamples = 1;
-        phase = 0;
+        vibrato
     end
     
     methods
-        function obj = Vibrato()
-            obj.pSR = getSampleRate(obj);
-            obj.DELAY = round(obj.Width * obj.pSR);
-            obj.WIDTH = round(obj.Width * obj.pSR);
-            obj.MODFREQ = obj.Modrate / obj.pSR;
-            obj.Buffer = zeros(192001,2);
-            obj.BufferIndex = 1;
-%             obj.NSamples = 1;
+        function obj = VibratoPlugin()
+           obj.vibrato = Vibrato(obj.rate, obj.width, getSampleRate(obj));         
         end
         
-        function set.Modrate(obj,Modrate)
-            obj.Modrate = Modrate;
-            fs = getSampleRate(obj);
-            obj.MODFREQ = Modrate / fs;
+        function set.rate(obj,rate)
+            obj.rate = rate;
+           % obj.vibrato.rate = obj.rate; 
+           setRate(obj.vibrato,rate);
         end
         
-        function set.Width(obj,Width)
-            obj.Width = Width;
-            fs = getSampleRate(obj);
-            obj.DELAY = round(Width * fs);
-            obj.WIDTH = round(Width * fs);
+        function set.width(obj,width)
+            obj.width = width;
+           % obj.vibrato.width = obj.width;
+            setWidth(obj.vibrato,width);
         end
         
         function reset(obj)
-            obj.pSR = getSampleRate(obj);
-%             obj.NSamples = 1;
-            obj.BufferIndex = 1;
-            obj.Buffer = zeros(192001,2);
+            reset(obj.vibrato, getSampleRate(obj));
         end
         
         function out = process(obj, x)
             y = zeros(size(x));
-            writeIndex = obj.BufferIndex;
-            
-            for i = 1:size(x,1)
-                obj.Buffer(writeIndex,:) = x(i,:); % Store buffer
-                
-                MOD = sin(obj.phase);
-                
-                delta = obj.MODFREQ * 2 * pi;
-                obj.phase = obj.phase + delta;
-                if obj.phase > 2 * pi
-                    obj.phase = obj.phase - 2*pi;
-                end
-                
-%                 obj.NSamples = obj.NSamples + 1;
-%                 if obj.NSamples > 192001
-%                     obj.NSamples = 1;
-%                 end
-                
-                TAP=1+obj.DELAY+obj.WIDTH*MOD;
-                n=floor(TAP);
-                frac = TAP - n;
-                readIndex = floor(writeIndex - n);
-                
-                if readIndex <= 0
-                    readIndex = readIndex + 192001;
-                end
-                if readIndex == 1
-                    y(i,:) = frac*obj.Buffer(192001) + (1-frac)*obj.Buffer(readIndex);
-                else
-                    y(i,:) = frac*obj.Buffer(readIndex - 1) + (1-frac)*obj.Buffer(readIndex);
-                end
-                writeIndex = writeIndex + 1;
-                if writeIndex > 192001
-                    writeIndex =  1;
-                end
-            end
-            obj.BufferIndex = writeIndex;
+            y = process(obj.vibrato, x);
             
             % Calculate output by adding wet and dry signal in appropriate
             % ratio
